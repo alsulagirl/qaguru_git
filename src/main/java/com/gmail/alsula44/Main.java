021883bf483ab3d66cc992aed7a75a3ed322667f
Alsou Shaydullina <35466659+alsulagirl@users.noreply.github.com>
2023-02-20 14:51:42 +0300

    Update Main.java

--- src/main/java/com/gmail/alsula44/Main.java
+++ src/main/java/com/gmail/alsula44/Main.java
@@ -1,10 +1,9 @@
 package com.gmail.alsula44;
 
 public class Main {
+    
+    private static String mag="Hello qa.guru";
+    
     public static void main(String[] args) {
-        System.out.println("Hello QAGURU!");
-        
-        
-        System.out.println("Hello QAGURU!");
-    }
+        System.out.println(msg);
 }
