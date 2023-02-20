a4e50d5d53aa761304ed6e93623cae7713f184ff
alsulagirl <alsula44@gmail.com>
2023-02-20 14:52:26 +0300

    added getMessage() method

--- src/main/java/com/gmail/alsula44/Main.java
+++ src/main/java/com/gmail/alsula44/Main.java
@@ -3,5 +3,11 @@ package com.gmail.alsula44;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello QAGURU!");
+
+        System.out.println("Bye QAGURU!");
+    }
+
+    private static String getMessage(){
+        return "Hello qa.guru";
     }
 }
