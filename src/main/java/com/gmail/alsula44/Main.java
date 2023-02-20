cb56d90cc7b3fb08a3de28e8c608e0455609b009
Alsou Shaydullina <35466659+alsulagirl@users.noreply.github.com>
2023-02-20 15:02:21 +0300

    added method getByeMessage()

--- src/main/java/com/gmail/alsula44/Main.java
+++ src/main/java/com/gmail/alsula44/Main.java
@@ -1,13 +1,21 @@
 package com.gmail.alsula44;
 
 public class Main {
-    private static String mag = "Hello qa.guru";
+    private static String msg = "Hello qa.guru";
+        
+    private static String msg2 = "Goodbye qa.guru";
 
     public static void main(String[] args) {
-        System.out.println(getMessage());
+        System.out.println(getHelloMessage());
+        
+        System.out.println(getByeMessage());
     }
 
-    private static String getMessage() {
-        return mag;
+    private static String getHelloMessage() {
+        return msg;
+    }
+    
+    private static String getByeMessage() {
+        return msg2;
     }
 }
