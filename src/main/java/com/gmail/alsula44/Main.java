b482df1eca01777cc6ec04a3871096de13dcb05b
Alsou Shaydullina <35466659+alsulagirl@users.noreply.github.com>
2023-02-21 07:57:51 +0300

    Update Main.java

--- src/main/java/com/gmail/alsula44/Main.java
+++ src/main/java/com/gmail/alsula44/Main.java
@@ -7,9 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
         
-        System.out.println(getByeMessage());
+        System.out.println(getHelloMessage()+getByeMessage());
     }
-
+    
+    private static String getByeMessage() {
+        return msg;
     
     private static String getByeMessage() {
         return msg2;
