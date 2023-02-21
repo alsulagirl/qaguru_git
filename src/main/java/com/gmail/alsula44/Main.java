ae60e623af791bd187d40f6602aa582be1a712f9
alsulagirl <alsula44@gmail.com>
2023-02-21 07:57:12 +0300

    added math method

--- src/main/java/com/gmail/alsula44/Main.java
+++ src/main/java/com/gmail/alsula44/Main.java
@@ -1,20 +1,15 @@
 package com.gmail.alsula44;
 
 public class Main {
-    private static String msg = "Hello qa.guru";
-        
     private static String msg2 = "Goodbye qa.guru";
 
     public static void main(String[] args) {
-        System.out.println(getHelloMessage());
-        
         System.out.println(getByeMessage());
+        int i = (int) (Math.random() * 10);
+
+        System.out.println("Случайное число:" + i);
     }
 
-    private static String getHelloMessage() {
-        return msg;
-    }
-    
     private static String getByeMessage() {
         return msg2;
     }
